68a334072c55fda1ae97fc599e92914604d39ee7
Shankar, Uma <Uma.Shankar@barco.com>
2018-03-20 13:52:43 +0530

    iniial setup

---
+++ XamrinFormsDemo/XamrinFormsDemo/XamrinFormsDemo/ViewModels/LoginViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamrinFormsDemo.ViewModels
+{
+    class LoginViewModel: BaseViewModel
+    {
+        public Action DisplayValidLoginPrompt;
+        public Action DisplayInvalidLoginPrompt;
+
+
+        string userName = string.Empty;
+        public string UserName
+        {
+            get { return userName; }
+            set { SetProperty(ref userName, value); }
+        }
+
+        string password = string.Empty;
+        public string Password
+        {
+            get { return password; }
+            set { SetProperty(ref password, value); }
+        }
+
+        string loginMessage = string.Empty;
+        public string LoginMessage
+        {
+            get { return loginMessage; }
+            set { SetProperty(ref loginMessage, value); }
+        }
+
+        public Command LoginCommand { get; protected set; }
+        public LoginViewModel()
+        {
+            Title = "Login";
+            LoginCommand = new Command(ExecuteLoginCommand);
+
+        }
+        private void ExecuteLoginCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                // Call API to login method
+                if(userName=="uma" && password == "uma")
+                {
+                    DisplayValidLoginPrompt();                  
+                }
+                else
+                {
+                    DisplayInvalidLoginPrompt();
+                }
+                loginMessage = "Loggged In Successfully.....:)";
+               
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+    }
+}
---
+++ XamrinFormsDemo/XamrinFormsDemo/XamrinFormsDemo/Views/LoginPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamrinFormsDemo.ViewModels;
+
+namespace XamrinFormsDemo.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+        LoginViewModel lvm;
+		public LoginPage ()
+		{
+            lvm = new LoginViewModel();
+            BindingContext = lvm;
+            lvm.DisplayValidLoginPrompt += () => DisplayAlert("Success", "Sucessfully Loggedin", "OK");
+            lvm.DisplayInvalidLoginPrompt += () => DisplayAlert("Error", "Invalid Login, try again", "OK");
+            InitializeComponent ();
+
+            UserName.Completed += (object sender, EventArgs e) =>
+            {
+                Password.Focus();
+            };
+
+            Password.Completed += (object sender, EventArgs e) =>
+            {
+                lvm.LoginCommand.Execute(null);
+            };
+
+        }
+	}
+}
